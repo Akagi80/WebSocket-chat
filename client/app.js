1d71ccd67749947b9d5a8d7deae8d7b2c5e35d96
Sebastian Sikora <ssikora80@gmail.com>
2021-07-03 16:55:24 +0200

    Add Socket.IO

--- client/app.js
+++ client/app.js
@@ -7,7 +7,11 @@ const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
 
-// Etap 4: Walidacja logowania
+const socket = io();
+// 28.3 dodajemy nasłuchowacz, wiadolość pojawia się na naszej LOKALNEJ liście
+socket.on('message', ({ author, content }) => addMessage(author, content))
+
+// 28.2 Etap 4: Walidacja logowania
 const login = e => {
   e.preventDefault();
   if (userNameInput.value) {
@@ -17,21 +21,21 @@ const login = e => {
   } else {
     alert('You have to type your name!')
   }
-
 }
 loginForm.addEventListener('submit', login);
 
 
-// Etap 5: Wysyłanie wiadomości
+// 28.2 Etap 5: Wysyłanie wiadomości
 const sendMessage = e => {
   e.preventDefault();
   if (messageContentInput.value) {
     addMessage(userName, messageContentInput.value);
+    // 28.3  dodajemy emitter aby wysłać ta wiadomość na serwer
+    socket.emit('message', { author: userName, content: messageContent })
     messageContentInput.value = '';
   } else {
     alert('You have to type message!')
   }
-  
 }
 addMessageForm.addEventListener('submit', sendMessage);
 
