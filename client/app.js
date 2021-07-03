d1f9b621954cf2c83d4e8478f39cf7a5a9dadafc
Sebastian Sikora <ssikora80@gmail.com>
2021-07-03 15:09:05 +0200

    Add new form functions

--- client/app.js
+++ client/app.js
@@ -7,6 +7,7 @@ const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
 
+// Etap 4: Walidacja logowania
 const login = e => {
   e.preventDefault();
   if (userNameInput.value) {
@@ -18,5 +19,30 @@ const login = e => {
   }
 
 }
-
 loginForm.addEventListener('submit', login);
+
+
+// Etap 5: Wysyłanie wiadomości
+const sendMessage = e => {
+  e.preventDefault();
+  if (messageContentInput.value) {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  } else {
+    alert('You have to type message!')
+  }
+  
+}
+addMessageForm.addEventListener('submit', sendMessage);
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <div class="message__content">${content}</div>
+  `;
+  messagesList.appendChild(message);
+}
